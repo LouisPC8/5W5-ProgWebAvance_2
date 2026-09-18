f6aa4cb10f3c509b9dc27c478c0f96253f1d58c7
Louis-Philippe Côté <6240460@cegepmontpetit.ca>
2026-09-18 17:44:39 -0400

    FCT ajout tache et connexion signalr

---
+++ PollingvsSignalR/PollingEtSignalR/labo.signalr.api/Hubs/MonHub.cs
@@ -0,0 +1,46 @@
+﻿using labo.signalr.api.Data;
+using labo.signalr.api.Models;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
+
+namespace labo.signalr.api.Hubs
+{
+    public class MonHub : Hub
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public MonHub(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SendTaskList()
+        {
+           List<UselessTask> value = await _context.UselessTasks.ToListAsync();
+           await Clients.Caller.SendAsync("tasklist", value);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await SendTaskList();
+        }
+
+        public async Task AddTask(string taskName)
+        {
+           
+            UselessTask uselessTask = new UselessTask()
+            {
+                Completed = false,
+                Text = taskName
+            };
+            await _context.UselessTasks.AddAsync(uselessTask);
+            await _context.SaveChangesAsync();
+            await Clients.Caller.SendAsync("taskAdded", uselessTask);
+            await SendTaskList();
+
+        }
+
+    }
+}
--- PollingvsSignalR/polling-et-signalr/app/signalr/page.tsx
+++ PollingvsSignalR/polling-et-signalr/app/signalr/page.tsx
@@ -4,23 +4,46 @@ import React from "react";
 import { useEffect } from "react";
 import { UselessTask } from "../models/UselessTask";
 import TaskView from "../_components/tasks-view";
+import { HubConnection, HubConnectionBuilder } from "@microsoft/signalr";
 
 export default function Home() {
 
   const [tasks, setTasks] = React.useState<UselessTask[]>([]);
+  const [hubConnection, setHubConnection] = React.useState<HubConnection>();
 
   useEffect(() => {
       connecttohub();
     }, []);
 
   function connecttohub() {
-    let testTasks = new Array<UselessTask>(
-          { id: 1, text: "Test Task 1", completed: false },
-          { id: 2, text: "Test Task 2", completed: true });
-        setTasks(testTasks);
+    // let testTasks = new Array<UselessTask>(
+    //       { id: 1, text: "Test Task 1", completed: false },
+    //       { id: 2, text: "Test Task 2", completed: true });
+    //     setTasks(testTasks);
     // TODO On doit commencer par créer la connexion vers le Hub
+      let newHubConnection = new HubConnectionBuilder().withUrl('http://localhost:5042/MonHub').build();
     // TODO On peut commencer à écouter pour les évènements qui vont déclencher des callbacks
+    newHubConnection.on('tasklist',(data) => {
+      console.log(data);
+      let taskList = new Array<UselessTask>();
+      for( let i = 0 ; i < data.length ; i++ ){
+        taskList.push(data[i])
+      }
+      setTasks(taskList);
+    })
+
+    newHubConnection.on('taskAdded', (data) => {
+      console.log(`La tâche: ${data} a été ajoutée`)
+    })
     // TODO On doit ensuite se connecter
+    newHubConnection
+            .start()
+            .then(()=>{
+              console.log('connection active');
+            })
+            .catch(err => console.log('erreur loers du lancement de la connection : ' + err));
+
+            setHubConnection(newHubConnection);
   }
 
   function onTaskToggle(id: number) {
@@ -30,8 +53,15 @@ export default function Home() {
     setTasks(tasksCopy);
   }
 
-  function handleTaskAdd() {
+  function handleTaskAdd(taskName : string) {
     // TODO On invoke la méthode pour ajouter une tâche sur le serveur
+    if(hubConnection){
+    hubConnection.invoke('AddTask',taskName)
+    .catch(err => console.error("erreur: " + err));
+    }else{
+      console.log("pas de co")
+    }
+    
   }
 
   return (
